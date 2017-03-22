42853bdc83ba2da1b9483d9f0fcd2a71017efdea
Anatoliy Klots and Max Reichman <max@>
2017-03-22 10:01:27 -0700

    Add form, and script.

--- shouting-machine/js/scripts.js
+++ shouting-machine/js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var phraseInput = $("input#phrase").val();
 
-    $(".phrase").text(phraseInput).toUppercase();
+    $(".phrase").text(phraseInput.toUpperCase());
 
     $("#shout").show();
 
